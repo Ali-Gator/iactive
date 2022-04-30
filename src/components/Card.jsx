52ee7aeced71328b5f826de0f579649cfc37c1b5
Oleg Gordienov <ogordienov@yandex.ru>
2022-05-01 00:41:36 +0200

    feat: add fetch, sorting, storage cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,51 +1,67 @@
 import avatar from '../images/avatar.png';
 import picture from '../images/picture.png';
+import {useEffect, useState} from 'react';
 
 
+export function Card({author, content, date, id}) {
+    const dateObj = new Date(date);
+    const dateStr = `${dateObj.getHours()}:${dateObj.getMinutes()}`;
+    const [favourite, setFavourite] = useState(false);
 
-export function Card() {
-    return <article className="card">
-        <img className="card__user-avatar" src={avatar}/>
-        <p className="card__author">Nina Malofeeva</p>
-        <ul className="card__buttons">
-            <li className="card__button-item">
-                <button className="card__button">Левый</button>
-            </li>
-            <li className="card__button-item">
-                <button className="card__button">Центр</button>
-            </li>
-            <li className="card__button-item">
-                <button className="card__button">Правый</button>
-            </li>
-        </ul>
-        <ul className="card__icons">
-            <li className="card__icon-item card__icon-item_type_send">
-                <button className="card__icon-button"></button>
-            </li>
-            <li className="card__icon-item card__icon-item_type_hide">
-                <button className="card__icon-button"></button>
-            </li>
-            <li className="card__icon-item card__icon-item_type_param">
-                <button className="card__icon-button"></button>
-            </li>
-            <li className="card__icon-item card__icon-item_type_favorite">
-                <button className="card__icon-button"></button>
-            </li>
-        </ul>
-        <span className="card__time">15:57</span>
-        <p className="card__text">"Россия 1" не боится снимать сериалы о сложных и
-            неоднозначных периодах в истории нашей страны. Это и "В круге первом", и "Жизнь и
-            судьба", и "Зулейха". Идёт работа над "Одним днём Ивана Денисовича". Вопрос Антону
-            Златопольскому -почему вы считаете</p>
-        <button className="card__next-button">Далее</button>
-        <img src={picture} alt="" className="card__picture"/>
-        <ul className="card__tags">
-            <li className="card__tag">
-                <button className="card__tag-button card__tag-button_active">#Новое</button>
-            </li>
-            <li className="card__tag">
-                <button className="card__tag-button">#Эксперт</button>
-            </li>
-        </ul>
-    </article>;
+    function handleFavouriteClick() {
+        setFavourite(!favourite);
+    }
+
+    useEffect(() => {
+            setFavourite(localStorage.getItem(`favourite${id}`) === 'true');
+
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem(`favourite${id}`, `${favourite}`);
+    }, [favourite]);
+
+    return <li className="messages__item" id={id}>
+        <article className="card">
+            <img className="card__user-avatar" src={avatar}/>
+            <p className="card__author">{author}</p>
+            <ul className="card__buttons">
+                <li className="card__button-item">
+                    <button className="card__button">Левый</button>
+                </li>
+                <li className="card__button-item">
+                    <button className="card__button">Центр</button>
+                </li>
+                <li className="card__button-item">
+                    <button className="card__button">Правый</button>
+                </li>
+            </ul>
+            <ul className="card__icons">
+                <li className="card__icon-item card__icon-item_type_send">
+                    <button className="card__icon-button"></button>
+                </li>
+                <li className="card__icon-item card__icon-item_type_hide">
+                    <button className="card__icon-button"></button>
+                </li>
+                <li className="card__icon-item card__icon-item_type_param">
+                    <button className="card__icon-button"></button>
+                </li>
+                <li className={`card__icon-item card__icon-item_type_favorite ${favourite ? 'card__icon-item_type_favorite-active' : ''}`}>
+                    <button className="card__icon-button" onClick={handleFavouriteClick}></button>
+                </li>
+            </ul>
+            <span className="card__time">{dateStr}</span>
+            <p className="card__text">{content}</p>
+            <button className="card__next-button">Далее</button>
+            <img src={picture} alt="" className="card__picture"/>
+            <ul className="card__tags">
+                <li className="card__tag">
+                    <button className="card__tag-button card__tag-button_active">#Новое</button>
+                </li>
+                <li className="card__tag">
+                    <button className="card__tag-button">#Эксперт</button>
+                </li>
+            </ul>
+        </article>
+    </li>;
 }
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,11 +1,63 @@
 import {Card} from './Card';
+import React, {useState} from 'react';
 
 export function Messages() {
+    const [posts, setPosts] = useState([]);
+    const [lastCardId, setLastCardId] = useState(0);
+    const [time, setTime] = useState(0);
+    const [direction, serDirection] = useState(true);
+
+    function getFormData(cardId = 0) {
+        let formData = new FormData();
+        formData.append('actionName', 'MessagesLoad');
+        formData.append('messageId', cardId.toString());
+        return formData;
+    }
+
+    function getPosts() {
+        return fetch('http://f0665380.xsph.ru', {
+            method: 'POST',
+            body: getFormData(lastCardId)
+        })
+            .then(res => res.ok ? res.json() : Promise.reject(`Ошибка ${res.status}`))
+            .then(data => {
+                if (data.Messages) {
+                    setPosts((prevPosts) => {
+                        if (prevPosts.length > 0) {
+                            for (let i = 0; i < data.Messages.length; i++) {
+                                if (!prevPosts.some(prevPost => prevPost.id === data.Messages[i].id)) {
+                                    direction
+                                        ? prevPosts.push(data.Messages[i])
+                                        : prevPosts.unshift(data.Messages[i]);
+                                }
+                            }
+                            return prevPosts;
+                        } else {
+                            return data.Messages;
+                        }
+                    });
+                    setLastCardId(data.Messages[data.Messages.length - 1].id);
+                }
+            })
+            .catch(err => console.log(err));
+    }
+
+    React.useEffect(() => {
+        setTimeout(() => setTime(new Date()), 5000);
+        getPosts();
+    }, [time]);
+
+    function handleDirection() {
+        serDirection(!direction);
+        setPosts(posts.reverse());
+    }
+
     return <section className="messages">
+        <button onClick={handleDirection}>Переключить ↑↓</button>
         <ul className="messages__list">
-            <li className="messages__item">
-                <Card/>
-            </li>
+            {posts.map(post => (
+                <Card key={post.id} author={post.author} content={post.content} date={post.date} id={post.id}/>
+            ))}
         </ul>
     </section>;
 }
